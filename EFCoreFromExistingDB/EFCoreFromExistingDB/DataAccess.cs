7a6f528014d429b9f5a82d20ac780f78b14f08be
RealMarkSussex <mark.sussex@gocompare.com>
2020-03-19 11:33:32 +0000

    added dataaccess class

--- EFCoreFromExistingDB/EFCoreFromExistingDB/DataAccess.cs
+++ EFCoreFromExistingDB/EFCoreFromExistingDB/DataAccess.cs
@@ -1,10 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using EFCoreFromExistingDB.Interfaces;
+using EFCoreFromExistingDB.Models;
+using EFCoreFromExistingDB.Repositories;
 
 namespace EFCoreFromExistingDB
 {
-    class DataAccess
+    public class DataAccess : IDataAccess
     {
+        public IEnumerable<Level> GetLevels() => LevelRepo.Get();
+
+        public void Add(Level level) => LevelRepo.Add(level);
+
+        public void Delete(Level level) => LevelRepo.Delete(level);
+
+        public void Update(Level level) => LevelRepo.Delete(level);
+
+
+        public IEnumerable<Role> GetRoles() => RoleRepo.Get();
+
+        public void Add(Role role) => RoleRepo.Add(role);
+
+        public void Delete(Role role) => RoleRepo.Delete(role);
+
+        public void Update(Role role) => RoleRepo.Update(role);
+
+
+        public IEnumerable<Skill> GetSkills() => SkillRepo.Get();
+
+        public void Add(Skill skill) => SkillRepo.Add(skill);
+
+        public void Delete(Skill skill) => SkillRepo.Delete(skill);
+
+        public void Update(Skill skill) => SkillRepo.Update(skill);
+
+
+        public IEnumerable<SkillLevel> GetSkillLevels() => SkillLevelRepo.Get();
+
+        public void Add(SkillLevel skillLevel) => SkillLevelRepo.Add(skillLevel);
+
+        public void Delete(SkillLevel skillLevel) => SkillLevelRepo.Delete(skillLevel);
+
+        public void Update(SkillLevel skillLevel) => SkillLevelRepo.Update(skillLevel);
+
+
+        public IEnumerable<User> GetUsers() => UserRepo.Get();
+
+        public void Add(User user) => UserRepo.Add(user);
+
+        public void Delete(User user) => UserRepo.Delete(user);
+
+        public void Update(User user) => UserRepo.Update(user);
+
+
+        public IEnumerable<UserSkill> GeUserSkills() => UserSkillRepo.Get();
+
+        public void Add(UserSkill userSkill) => UserSkillRepo.Add(userSkill);
+
+        public void Delete(UserSkill userSkill) => UserSkillRepo.Delete(userSkill);
+
+        public void Update(UserSkill userSkill) => UserSkillRepo.Update(userSkill);
     }
 }
--- EFCoreFromExistingDB/EFCoreFromExistingDB/Interfaces/IDataAccess.cs
+++ EFCoreFromExistingDB/EFCoreFromExistingDB/Interfaces/IDataAccess.cs
@@ -1,10 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using EFCoreFromExistingDB.Models;
 
 namespace EFCoreFromExistingDB.Interfaces
 {
-    interface DataAccess
+    public interface IDataAccess
     {
+        IEnumerable<Level> GetLevels();
+        void Add(Level level);
+        void Delete(Level level);
+        void Update(Level level);
+
+
+        IEnumerable<Role> GetRoles();
+        void Add(Role role);
+        void Delete(Role role);
+        void Update(Role role);
+
+
+        IEnumerable<Skill> GetSkills();
+        void Add(Skill skill);
+        void Delete(Skill skill);
+        void Update(Skill skill);
+
+
+        IEnumerable<SkillLevel> GetSkillLevels();
+        void Add(SkillLevel skillLevel);
+        void Delete(SkillLevel skillLevel);
+        void Update(SkillLevel skillLevel);
+
+
+        IEnumerable<User> GetUsers();
+        void Add(User user);
+        void Delete(User user);
+        void Update(User user);
+
+
+        IEnumerable<UserSkill> GeUserSkills();
+        void Add(UserSkill userSkill);
+        void Delete(UserSkill userSkill);
+        void Update(UserSkill userSkill);
     }
 }
