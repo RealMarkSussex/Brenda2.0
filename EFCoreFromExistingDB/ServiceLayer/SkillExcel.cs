9873a39094b98cadc739f9c6546993e693023d75
RealMarkSussex <mark.sussex@gocompare.com>
2020-04-01 16:20:13 +0100

    small correction

--- EFCoreFromExistingDB/ServiceLayer/SkillExcel.cs
+++ EFCoreFromExistingDB/ServiceLayer/SkillExcel.cs
@@ -52,7 +52,7 @@ namespace ServiceLayer
                 var skill = _skills[r - 1];
                 worksheet.Cells[r, 0].Style.Borders.SetBorders(MultipleBorders.Outside, Color.Black, LineStyle.Thin);
                 worksheet.Cells[r, 1].Style.Borders.SetBorders(MultipleBorders.Outside, Color.Black, LineStyle.Thin);
-                worksheet.Cells[r, 0].Style.Borders.SetBorders(MultipleBorders.Outside, Color.Black, LineStyle.Thin);
+                worksheet.Cells[r, 2].Style.Borders.SetBorders(MultipleBorders.Outside, Color.Black, LineStyle.Thin);
                 worksheet.Cells[r, 0].Value = skill.SkillId;
                 worksheet.Cells[r, 1].Value = skill.Name;
                 worksheet.Cells[r, 2].Value = skill.Description;
